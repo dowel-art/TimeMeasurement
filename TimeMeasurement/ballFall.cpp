3b1e3924a22ea67746c2d9509bccf4277bf20cd4
dowel-art <65223199+dowel-art@users.noreply.github.com>
2020-09-03 21:39:05 +0900

    変数名を変更

--- TimeMeasurement/ballFall.cpp
+++ TimeMeasurement/ballFall.cpp
@@ -14,27 +14,27 @@ void ballFall::ledFlash() {
 //投擲装置のソレノイド制御
 void ballFall::ballDrop(bool state) {
   if (state == true)
-    digitalWrite(PIN_SOL_FALL, HIGH);
+    digitalWrite(PIN_SOL_DROP, HIGH);
   else
-    digitalWrite(PIN_SOL_FALL, LOW);
+    digitalWrite(PIN_SOL_DROP, LOW);
 }
 
 //送り装置のソレノイド制御
 void ballFall::ballRoad(bool state) {
   if (state == true)
-    digitalWrite(PIN_SOL_LINE, HIGH);
+    digitalWrite(PIN_SOL_ROAD, HIGH);
   else
-    digitalWrite(PIN_SOL_LINE, LOW);
+    digitalWrite(PIN_SOL_ROAD, LOW);
 }
 
 //ソレノイドリセット
 void ballFall::solReset() {
-  digitalWrite(PIN_SOL_LINE, LOW);
-  digitalWrite(PIN_SOL_FALL, LOW);
+  digitalWrite(PIN_SOL_ROAD, LOW);
+  digitalWrite(PIN_SOL_DROP, LOW);
 }
 
 //通過センサのチェック
 bool ballFall::ballCheck() {
-  if (analogRead(PIN_SENSOR) < THRESHOLD) return true;
+  if (analogRead(PIN_BALL_SENSOR) < THRESHOLD_BALL) return true;
   else return false;
 }
