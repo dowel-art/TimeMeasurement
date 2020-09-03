c39040a7d599f1b7d3c5185a91f86c70ee3da64e
dowel-art <65223199+dowel-art@users.noreply.github.com>
2020-09-03 21:40:16 +0900

    変数名を修正

--- TimeMeasurement/define.h
+++ TimeMeasurement/define.h
@@ -3,21 +3,21 @@
 #define PIN_DIR 2
 #define PIN_STEP 3
 #define PIN_CS 4
-#define PIN_SOL_FALL 5
-#define PIN_SOL_LINE 6
+#define PIN_SOL_DROP 5
+#define PIN_SOL_ROAD 6
 #define PIN_SW 7
 #define PIN_LED 8
 
-#define PIN_SENSOR A0
-#define PIN_SENSOR A1
+#define PIN_PLATE_SENSOR A0
+#define PIN_BALL_SENSOR A1
 
-#define THRESHOLD_SENSOR 800
+#define THRESHOLD_PLATE 800
+#define THRESHOLD_BALL 980
 #define DIR_FLIP 0
 #define DIR_RESET 1
-#define THRESHOLD 980
-#define TIMEOUT 2000
+#define TIMEOUT 3000
 
-#define StepPeriodUs_flip 4
-#define StepPeriodUs_reset 80
+#define StepPeriodUs_flip 30
+#define StepPeriodUs_reset 30
 
-#define FLIP_DELAY 10000000 //us
+#define FLIP_DELAY 330 //ボール落下からフリップするまでの時間
